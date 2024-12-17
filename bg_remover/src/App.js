e90883425943ef08369977a96c804564c0592605
Sambhaji Sanjiv Ajane <ajanesambhaji@gmail.com>
2024-12-17 16:17:17 -0600

    basic setup done

---
+++ bg_remover/src/App.js
@@ -0,0 +1,12 @@
+import BgRemover from './BgRemover';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ImgInput /> */}
+      <BgRemover />
+    </div>
+  );
+}
+
+export default App;
